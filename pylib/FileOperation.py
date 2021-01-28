7a6a3c68089a41281d3219dd179e3853497e7f1f
JZJY <1531820147@qq.com>
2021-01-28 17:39:36 +0800

    yield.send()方法运行

--- pylib/FileOperation.py
+++ pylib/FileOperation.py
@@ -5,9 +5,21 @@
 @Function: 文件操作类
 """
 
-class FileOperation:
-    pass
 
-    def __init__(self):
-        print('shop')
-        print(2)
+def foo(num):
+    print("starting...")
+    while num < 5:
+        res = yield num
+        print(res)
+        num = num + 1
+# def foo():
+#     print("starting...")
+#     while True:
+#         res = yield 4
+#         print("res:",res)
+
+g = foo(0)
+print(g.__next__())
+print(g.send(888))
+# print(g.__next__())
+
